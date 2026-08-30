e94a5358f54c2dd428e1a1555beebe480489c79a
SalzDevs <a49000@alunos.isel.pt>
2026-08-30 21:22:28 +0100

    read files from disk

--- main.go
+++ main.go
@@ -1,6 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"gopkg.in/yaml.v3"
+	"os"
+)
 
 func main() {
-	println("Hello Seamen!!");
+	content, err := os.ReadFile("./test.yaml");
+	if err!=nil { 
+		log.Fatalf("error:%v",err);
+	}
+
+	parsedData := make(map[string] any);
+	
+
+	err = yaml.Unmarshal([]byte(content), &parsedData);
+
+	fmt.Printf("ParsedData: %v\n",parsedData);
+
+	if err!=nil {
+		log.Fatalf("error: %v",err);
+	}
+
+	for k,v := range parsedData {
+		fmt.Printf("(k:%s) (v:%v)\n",k,v);	
+	}
+
+
 }
