43e014da40703555f7db76839a196d60840f4da9
SalzDevs <a49000@alunos.isel.pt>
2026-08-31 21:00:09 +0100

    refactor load yaml code into dedicated function

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use std::fs;
 use std::env;
 use std::path::PathBuf;
+use std::io;
+
+
+fn load_yaml(path: &str) -> Result<String, io::Error> {
+    let root_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let file_path = root_dir.join(path);
+    
+    let contents = fs::read_to_string(file_path)?;
+
+    return Ok(contents);
+}
 
 fn main() -> std::io::Result<()> {
-    let root_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    
-    let file_path = root_dir.join("test.yaml");
-    println!("{}", file_path.display());
 
-    let contents = fs::read_to_string(file_path)?;
-    
+    let contents = load_yaml("test.yaml")?;    
     println!("{contents}");
 
     return Ok(());
