304d54a860fe5a1b4dd85ee6c6539dfbf73929d8
SalzDevs <a49000@alunos.isel.pt>
2026-09-04 18:46:03 +0100

    rmeove go files and start port to rust

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,20 @@ use std::env;
 use std::path::PathBuf;
 use std::io;
 
-
-fn load_yaml(path: &str) -> Result<String, io::Error> {
+//TODO: Need to validate if file is valid yaml
+fn load_yaml(file_name: &str) -> Result<String, io::Error> {
     let root_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let file_path = root_dir.join(path);
-    
+    let file_path = root_dir.join(file_name);
+
     let contents = fs::read_to_string(file_path)?;
 
     return Ok(contents);
 }
 
+
 fn main() -> std::io::Result<()> {
-
-    let contents = load_yaml("test.yaml")?;    
-    println!("{contents}");
-
+    let file_name = "test.yaml";
+    let contents = load_yaml(file_name)?;
+    println!("contents: {contents}");
     return Ok(());
 }
